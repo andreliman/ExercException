bb625ff72624b2198501a664af261eb058acf5eb
andreliman <andredelimanunes@gmail.com>
2022-03-22 20:19:48 -0300

    Account class created and logic implemented

--- ExercException/Program.cs
+++ ExercException/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ExercException.Entities;
+using ExercException.Entities.Exceptions;
+using System;
+using System.Globalization;
 
 namespace ExercException
 {
@@ -6,7 +9,32 @@ namespace ExercException
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter account data");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Holder: ");
+            string holder = Console.ReadLine();
+            Console.Write("Initial balance: ");
+            double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Withdraw limit: ");
+            double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+
+            Account account = new Account(number, holder, balance, withdrawLimit);
+
+            Console.Write("Enter amount for withdraw: ");
+            double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            try
+            {
+                account.Withdraw(amount);
+                Console.WriteLine($"New balance: {account.Balance.ToString("F2", CultureInfo.InvariantCulture)}");
+
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
