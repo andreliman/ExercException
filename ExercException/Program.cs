24cd73688ba1ebf8ad876c03bb3b8671333ecb15
andreliman <andredelimanunes@gmail.com>
2022-03-22 14:32:29 -0300

    Add project files.

---
+++ ExercException/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExercException
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
